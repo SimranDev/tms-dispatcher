83ec252fa3901cc9bb9bb6bd856b33d30c9bcb18
Simran <simran.develop@gmail.com>
2025-07-18 20:48:41 +1200

    feat: integrate React Query and restructure router for authentication flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { RouterProvider } from 'react-router'
 import { router } from './router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function App() {
+  const queryClient = new QueryClient()
+
   return (
     <>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   )
 }
--- src/router.tsx
+++ src/router.tsx
@@ -1,29 +1,44 @@
-import { createBrowserRouter } from 'react-router'
+import { createBrowserRouter, Navigate } from 'react-router'
 import DashboardPage from './pages/DashboardPage'
 import Layout from './components/layout/Layout'
 import { PageRoute } from './types/components'
 import JobsPage from './pages/JobsPage'
+import ProtectedRoute from './components/ProtectedRoute'
+import LoginPage from './pages/LoginPage'
 
 export const router = createBrowserRouter([
   {
-    Component: Layout,
+    path: '/login',
+    Component: LoginPage,
+  },
+  {
+    Component: ProtectedRoute,
     children: [
       {
-        index: true,
-        path: PageRoute.Dashboard,
-        Component: DashboardPage,
+        path: '/',
+        element: <Navigate to={PageRoute.Dashboard} replace />,
       },
       {
-        path: PageRoute.Jobs,
-        Component: JobsPage,
-      },
-      {
-        path: PageRoute.Vehicles,
-        element: <h1>VEHICLES</h1>,
-      },
-      {
-        path: PageRoute.Drivers,
-        element: <h1>DRIVERS</h1>,
+        Component: Layout,
+        children: [
+          {
+            index: true,
+            path: PageRoute.Dashboard,
+            Component: DashboardPage,
+          },
+          {
+            path: PageRoute.Jobs,
+            Component: JobsPage,
+          },
+          {
+            path: PageRoute.Vehicles,
+            element: <h1>VEHICLES</h1>,
+          },
+          {
+            path: PageRoute.Drivers,
+            element: <h1>DRIVERS</h1>,
+          },
+        ],
       },
     ],
   },
