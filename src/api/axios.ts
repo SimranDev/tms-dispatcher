c9db7d55ad9b38ce2faf3c2ab2952a0d87f8c176
Simran <simran.develop@gmail.com>
2025-07-19 11:15:57 +1200

    feat: add JWT token interceptor for API requests and implement drivers API

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -7,6 +7,20 @@ const instance = axios.create({
   },
 })
 
+// Add request interceptor to include JWT token as bearer token
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
 export const api = {
   get: (url: string, config?: AxiosRequestConfig) => instance.get(url, config).then((res) => res.data),
   post: (url: string, body: unknown, config?: AxiosRequestConfig) => instance.post(url, body, config).then((res) => res.data),
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,11 @@
-import type { Job } from '../types/components'
+import type { Driver, Job } from '../types/components'
 import { api } from './axios'
 
 export const jobsAPI = {
   getJobs: () => api.get('/jobs'),
   createJob: (job: Job): Promise<Job> => api.post('/jobs', job),
+}
+
+export const driversAPI = {
+  getDrivers: (): Promise<Driver[]> => api.get('/drivers'),
 }
